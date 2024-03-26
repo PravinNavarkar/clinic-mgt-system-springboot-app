e81f373bde1f96e6f8e1960f74cd085f3824c5ec
Pravin Navarkar <pravin.navarkar@tenerity.com>
2024-03-26 10:59:41 +0530

    Modified controller, repository and serviceImpl files

--- src/main/java/com/example/mgt/controller/ClinicController.java
+++ src/main/java/com/example/mgt/controller/ClinicController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Component
+@RestController
 @RequestMapping("/clinics")
 public class ClinicController {
     @Autowired
--- src/main/java/com/example/mgt/repository/ClinicRepository.java
+++ src/main/java/com/example/mgt/repository/ClinicRepository.java
@@ -4,7 +4,8 @@ import com.example.mgt.domain.Clinic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 
-@Controller
+@Repository
 public interface ClinicRepository extends JpaRepository<Clinic,Integer> {
 }
